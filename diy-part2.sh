88201ee6830eaf9a24fe72b4526c90b0ea26cb17
king stark <63186186+King-stark@users.noreply.github.com>
2022-05-31 15:19:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+rm -rf package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
