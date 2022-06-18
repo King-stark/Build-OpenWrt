1382d25959ac68d7b11327814eebf2330ca5b7e4
king stark <63186186+King-stark@users.noreply.github.com>
2022-06-18 21:44:06 +0800

    Update diy-kenzok1.sh

--- diy-kenzok1.sh
+++ diy-kenzok1.sh
@@ -3,6 +3,5 @@
 # 拉取lede源码后，自定义feeds
 
 # 添加kenzok8 的 luci包
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
