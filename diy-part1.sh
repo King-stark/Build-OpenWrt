94113e6ae1801e3a4ce2cb15c6665472c4da3fd8
king stark <63186186+King-stark@users.noreply.github.com>
2022-06-20 08:44:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #src-git xepher https://github.com/jerrykuku/luci-app-ttnode.git
-cat >> feeds.conf.default <<EOF
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-EOF
+#cat >> feeds.conf.default <<EOF
+#src-git kenzo https://github.com/kenzok8/openwrt-packages
+#src-git small https://github.com/kenzok8/small
+#EOF
